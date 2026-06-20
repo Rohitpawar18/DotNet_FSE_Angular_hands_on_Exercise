57409819cef75008da84bdffc23778996ef2e50d
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-06-20 23:31:07 +0530

    AdapterPatternExample Done

---
+++ Design Patterns and Principles Exercise Solved/AdapterPatternExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public interface IPaymentProcessor {
+    void ProcessPayment();
+}
+
+public class PayPalGateway {
+    public void SendPayment() {
+        Console.WriteLine("Payment processed through PayPal.");
+    }
+}
+
+public class PaymentAdapter : IPaymentProcessor { 
+    private PayPalGateway gateway;
+
+    public PaymentAdapter(PayPalGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public void ProcessPayment()
+    {
+        gateway.SendPayment();
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        PayPalGateway gateway = new PayPalGateway();
+
+        IPaymentProcessor payment = new PaymentAdapter(gateway);
+
+        payment.ProcessPayment();
+
+        Console.ReadKey();
+    }
+}

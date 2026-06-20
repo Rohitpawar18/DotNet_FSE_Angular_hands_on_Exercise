3092e1cc10398384db613bae9b6dfec1a23c19f9
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-06-20 20:51:03 +0530

    BuilderPatternExample done with output

---
+++ BuilderPatternExample/Computer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPatternExample
+{
+    public class Computer
+    {
+        private string CPU;
+        private string RAM;
+        private string Storage;
+
+        //private constructor
+        private Computer(ComputerBuilder builder)
+        {
+            CPU = builder.CPU;
+            RAM = builder.RAM;
+            Storage = builder.Storage;
+        }
+
+        //Builder class
+        public class ComputerBuilder
+        {
+            public string CPU;
+            public string RAM;
+            public string Storage;
+
+            //Required Fields
+            public ComputerBuilder(string cpu)
+            {
+                CPU = cpu;
+            }
+
+            public ComputerBuilder SetRAM (string ram)
+            {
+                RAM = ram;
+                return this;
+            }
+
+            public ComputerBuilder SetStorage (string storage)
+            {
+                Storage = storage;
+                return this;
+            }
+
+            public Computer Build()
+            {
+                return new Computer(this);
+            }
+        }
+
+        public void showDetails() {
+            Console.WriteLine("Computer Configuration :");
+            Console.WriteLine("CPU: "+ CPU);
+            Console.WriteLine("RAM: " + RAM);
+            Console.WriteLine("Storage: " + Storage);
+        }
+    }
+}
---
+++ BuilderPatternExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BuilderPatternExample
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Computer gamingPC = new Computer.ComputerBuilder("Intel i7")
+                                            .SetRAM("16 GB")
+                                            .SetStorage("1TB SSD")
+                                            .Build();
+            gamingPC.showDetails();
+
+            Console.WriteLine();
+
+            Computer officePC = new Computer.ComputerBuilder("Intel i5")
+                                            .SetRAM("8 GB")
+                                            .SetStorage("512GB SSD")
+                                            .Build();
+            officePC.showDetails();
+
+            Console.ReadKey();
+        }
+    }
+}

c00b014de84b62427441b36187094bde02d0dbdb
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-06-21 11:52:55 +0530

    ProxyPatternExample Done

---
+++ Design Patterns and Principles Exercise Solved/ProxyPatternExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public interface IImage
+{ 
+    void Display();
+}
+
+//Real Object
+public class RealImage : IImage
+{
+    private string fileName;
+
+    public RealImage(string fileName)
+    {
+        this.fileName = fileName;
+        LoadFromServer();
+    }
+
+    private void LoadFromServer() {
+        Console.WriteLine("Loading Image from remote server : "+fileName);
+    }
+
+    public void Display() {
+        Console.WriteLine("Displaying image : " + fileName);
+    }
+}
+
+//Proxy Class
+public class ProxyImage : IImage
+{
+    private RealImage realImage;
+    private string fileName;
+
+    public ProxyImage(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public void Display() { 
+        //Lazy Initialization
+        if(realImage == null)
+        {
+            realImage = new RealImage(fileName);
+        }
+
+        realImage.Display();
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        IImage image = new ProxyImage("Nature.jpg");
+
+        Console.WriteLine("Image Object Created");
+
+        Console.WriteLine("\nFirst Display Call : ");
+        image.Display();
+
+        Console.WriteLine("\nSecond Display Call : ");
+        image.Display();
+
+        Console.ReadKey();
+    }
+}

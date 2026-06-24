9c0f447f226a140df3a3420afbf11565bfa993b3
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-06-24 22:12:12 +0530

    NUnit-Handson Done

---
+++ Week 1 Solved Exercise/NUnitAndMoq Solved/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator = null;
+        }
+
+        [Test]
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 5, 10)]
+        [TestCase(100, 200, 300)]
+        public void Add_ReturnsExpectedResult(
+            int num1,
+            int num2,
+            int expectedResult)
+        {
+            int actualResult =
+                calculator.Add(num1, num2);
+
+            Assert.That(
+                actualResult,
+                Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ Week 1 Solved Exercise/NUnitAndMoq Solved/CalcLibrary/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}

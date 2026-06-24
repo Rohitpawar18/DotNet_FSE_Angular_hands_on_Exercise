2464a4a7ee80f61ee2adbf4bb8fa579a930f4289
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-06-24 18:41:38 +0530

    Write Testable Code with Moq Done

---
+++ Week 1 Solved Exercise/NUnitAndMoq Solved/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mailSenderMock = null!;
+        private CustomerCommLib.CustomerComm _customerComm = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mailSenderMock = new Mock<IMailSender>();
+
+            _mailSenderMock
+                .Setup(x => x.SendMail(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm =
+                _customerComm = new CustomerCommLib.CustomerComm(_mailSenderMock.Object);
+        }
+
+        [Test]
+        [TestCase]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            bool result =
+                _customerComm.SendMailToCustomer();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Week 1 Solved Exercise/NUnitAndMoq Solved/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            string customerEmail = "cust123@abc.com";
+            string message = "Some Message";
+
+            _mailSender.SendMail(customerEmail, message);
+
+            return true;
+        }
+    }
+}

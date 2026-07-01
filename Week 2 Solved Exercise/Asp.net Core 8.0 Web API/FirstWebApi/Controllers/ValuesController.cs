b84818b0bf6f1cb01d38093fd749d3cf70d90733
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-07-01 23:15:10 +0530

    First Web Api using .Net core

---
+++ Week 2 Solved Exercise/Asp.net Core 8.0 Web API/FirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static List<String> _values = new List<String> { 
+            "Value1", "Value2", "Value3"
+        };
+
+        // GET: api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_values);
+        }
+
+        // GET: api/values/1
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0 || id >= _values.Count)
+                return NotFound();
+
+            return Ok(_values[id]);
+        }
+
+        // POST: api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] string value)
+        {
+            _values.Add(value);
+            return CreatedAtAction(nameof(Get), new { id = _values.Count - 1 }, value);
+        }
+
+        // PUT: api/values/1
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            if (id < 0 || id >= _values.Count)
+                return NotFound();
+
+            _values[id] = value;
+            return NoContent();
+        }
+
+        // DELETE: api/values/1
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0 || id >= _values.Count)
+                return NotFound();
+
+            _values.RemoveAt(id);
+            return NoContent();
+        }
+    }
+}
